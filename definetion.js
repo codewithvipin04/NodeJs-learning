871c5400fa8771d60a10dc99002ae3679ce9f48e
codewithvipin04 <sikarwarvipin1996@gmail.com>
2026-03-13 11:50:55 +0530

    Node Js Definition only.
    Add comments explaining NodeJS features and characteristics.

---
+++ definetion.js
@@ -0,0 +1,5 @@
+/* NodeJS is an open-source, cross platform, Javascript runtime envirionment. 
+ Open source - publically available for sharing and modification.
+ Cross platform - Available on Mac, Windows and Linux.
+  Javascript runtime environment -
+*/
