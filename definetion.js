650c0c4c7306e1f5f6a0d5510570498c9185c168
codewithvipin04 <sikarwarvipin1996@gmail.com>
2026-03-13 12:07:01 +0530

    Fix spelling errors in definetion.js comments
    Corrected spelling errors in comments and improved clarity.

--- definetion.js
+++ definetion.js
@@ -1,5 +1,7 @@
-/* NodeJS is an open-source, cross platform, Javascript runtime envirionment. 
+/* What is NodeJs ?
+  NodeJS is an open-source, cross platform, Javascript runtime envirionment. 
    Open source - publically available for sharing and modification.
    Cross platform - Available on Mac, Windows and Linux.
-   Javascript runtime environment -
+   Javascript runtime environment - provides all the nessesary components in order to use and 
+   run javascript program outside the browser.
 */
