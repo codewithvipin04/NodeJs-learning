00b31f55597e9035f792956d0dc58292a29ba2a3
codewithvipin04 <sikarwarvipin1996@gmail.com>
2026-03-13 12:11:33 +0530

    Fix spelling errors and enhance Node.js description
    Corrected spelling errors and improved clarity.

--- definetion.js
+++ definetion.js
@@ -4,4 +4,14 @@
    Cross platform - Available on Mac, Windows and Linux.
    Javascript runtime environment - provides all the nessesary components in order to use and 
    run javascript program outside the browser.
+
+   What can you biuld with NodeJs ?
+    1. Traditional websites.
+    2. Backend services like Api's.
+    3. Real-time applications.
+    4. Streaming services like Netflix.
+    5. CLI Tools.
+    6. Multiplayer games.
+
+    NodeJs allow you to build complex and Powerfull application.
 */
